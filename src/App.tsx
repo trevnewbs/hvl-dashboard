854b0fc3375d5187f20d698a70bd08ab76c1e27e
trevnewbs <trevorhvl@MacBookAir.attlocal.net>
2025-01-03 16:42:07 -0600

    fix: Add explicit theme to ChakraProvider and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({})
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <div>Hello World</div>
     </ChakraProvider>
   )
