55bd11bff61814cd4eb1f5dee67c1ca9c20ebc57
trevnewbs <trevorhvl@MacBookAir.attlocal.net>
2025-01-03 16:39:35 -0600

    test: Simplify App.tsx to isolate ChakraProvider issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import { ChakraProvider, Box } from '@chakra-ui/react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-const queryClient = new QueryClient()
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
-        <Router>
-          <Box minH="100vh">
-            <Routes>
-              <Route path="/" element={<div>Welcome to HVL Dashboard!</div>} />
-            </Routes>
-          </Box>
-        </Router>
-      </ChakraProvider>
-    </QueryClientProvider>
+    <ChakraProvider>
+      <div>Hello World</div>
+    </ChakraProvider>
   )
 }
 
